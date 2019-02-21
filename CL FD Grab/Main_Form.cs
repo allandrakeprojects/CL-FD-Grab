bf2a0db44796441e301f17b6b395ffd003f05d74
SSIMAKATI\adulay <allandulay69@gmail.com>
2019-02-21 09:20:46 +0800

    update api

--- CL FD Grab/Main_Form.cs
+++ CL FD Grab/Main_Form.cs
@@ -397,7 +397,7 @@ namespace CL_FD_Grab
                         ["token"] = token
                     };
 
-                    var result = wb.UploadValues("http://zeus.ssimakati.com:8080/API/lastFDRecord", "POST", data);
+                    var result = wb.UploadValues("http://zeus.ssitex.com:8080/API/lastFDRecord", "POST", data);
                     string responsebody = Encoding.UTF8.GetString(result);
                     var deserializeObject = JsonConvert.DeserializeObject(responsebody);
                     JObject jo = JObject.Parse(deserializeObject.ToString());
@@ -863,7 +863,7 @@ namespace CL_FD_Grab
                         ["token"] = token
                     };
 
-                    var response = wb.UploadValues("http://zeus.ssimakati.com:8080/API/sendFD", "POST", data);
+                    var response = wb.UploadValues("http://zeus.ssitex.com:8080/API/sendFD", "POST", data);
                     string responseInString = Encoding.UTF8.GetString(response);
                 }
             }
@@ -922,7 +922,7 @@ namespace CL_FD_Grab
                         ["token"] = token
                     };
 
-                    var response = wb.UploadValues("http://zeus2.ssimakati.com:8080/API/sendFD", "POST", data);
+                    var response = wb.UploadValues("http://zeus2.ssitex.com:8080/API/sendFD", "POST", data);
                     string responseInString = Encoding.UTF8.GetString(response);
                 }
             }
@@ -1228,7 +1228,7 @@ namespace CL_FD_Grab
                         ["token"] = token
                     };
 
-                    var response = wb.UploadValues("http://zeus.ssimakati.com:8080/API/updateAppStatus", "POST", data);
+                    var response = wb.UploadValues("http://zeus.ssitex.com:8080/API/updateAppStatus", "POST", data);
                     string responseInString = Encoding.UTF8.GetString(response);
                 }
             }
@@ -1277,7 +1277,7 @@ namespace CL_FD_Grab
                         ["token"] = token
                     };
 
-                    var response = wb.UploadValues("http://zeus2.ssimakati.com:8080/API/updateAppStatus", "POST", data);
+                    var response = wb.UploadValues("http://zeus2.ssitex.com:8080/API/updateAppStatus", "POST", data);
                     string responseInString = Encoding.UTF8.GetString(response);
                 }
             }
